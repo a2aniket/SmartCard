e2f762fde79a72ff329dc26f161f34ea04abadb5
Aniket Thorat <thorataniket777@gmail.com>
2022-03-22 17:33:32 +0530

    distory maping at frount end

--- app.py
+++ app.py
@@ -5,18 +5,15 @@ import pandas as pd
 
 @app.route("/")
 def home():
-   #with open('data.p', 'rb') as handle:
-        #data = pickle.load(handle)
-    df=pd.read_csv("bill.csv")
-    total=df["total_price"].sum()
-    unit_total=df["unit"].sum()
-    Name=df["Name"]
-    length=len(Name)
-    description=df["description"]
-    price=df["price"]
-    unit=df["unit"]
-    total_price=df["total_price"]
-    image="Colgate.jpg"
-    return render_template("home.html",image=image,length=length,Name=Name,description=description,price=price,unit=unit,total_price=total_price,total=total,unit_total=unit_total)
+    with open('bill.pkl', 'rb') as handle:
+        b = pickle.load(handle)
+    total=0
+    units=0
+    for i in b:
+        total=total+b[i]['totalPrice']
+        units=units+b[i]['unit']
+    
+    
+    return render_template("home.html",bill=b,units=units,total=total)
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True, host= '192.168.15.167')
