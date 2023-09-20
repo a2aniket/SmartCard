7246fe12684fbdc3db897d112401404870192127
root <root@ip-172-31-89-134.ec2.internal>
2023-09-20 12:11:19 +0000

    Commit message-2023-09-20 12:11

---
+++ spring-server/src/main/java/com/persistent/model/GenerateCodePostRequest.java
@@ -0,0 +1,36 @@
+package com.persistent.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+/**
+ * GenerateCodePostRequest
+ */
+
+@JsonTypeName("_generate_code_post_request")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-09-20T12:11:19.052532203Z[UTC]")
+@Data
+public class GenerateCodePostRequest {
+
+  private String language;
+  
+  private String openAPIUrl;
+  
+
+
+}
