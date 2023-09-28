5d21f43a74f4e589bc920459aa6efc7e66ea4b3f
root <root@ip-172-31-89-134.ec2.internal>
2023-09-28 11:05:54 +0000

    Commit message-2023-09-28 11:05

--- spring-server/src/main/java/com/persistent/model/GenerateCodePost200ResponseDto.java
+++ spring-server/src/main/java/com/persistent/model/GenerateCodePost200ResponseDto.java
@@ -33,7 +33,7 @@ import javax.persistence.SequenceGenerator;
  */
 
 @JsonTypeName("_generate_code_post_200_response")
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-09-28T11:04:46.764923567Z[UTC]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-09-28T11:05:53.809116950Z[UTC]")
 @Data
 @Entity
 @AllArgsConstructor
--- spring-server/src/main/java/com/persistent/model/GenerateCodePostRequest.java
+++ spring-server/src/main/java/com/persistent/model/GenerateCodePostRequest.java
@@ -23,7 +23,7 @@ import javax.persistence.Id;
  */
 
 @JsonTypeName("_generate_code_post_request")
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-09-28T11:04:46.764923567Z[UTC]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-09-28T11:05:53.809116950Z[UTC]")
 @Data
 public class GenerateCodePostRequest {
 
