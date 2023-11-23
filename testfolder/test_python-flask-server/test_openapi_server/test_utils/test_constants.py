dca1ce4af31dc1a3c2b154b827976c7c41edd081
Anurag Jain <anurag_jain1@persistent.com>
2023-11-23 13:27:24 +0530

    TestGen generated unit tests - 2023-11-23 13:27

--- testfolder/test_python-flask-server/test_openapi_server/test_utils/test_constants.py
+++ testfolder/test_python-flask-server/test_openapi_server/test_utils/test_constants.py
@@ -1,7 +1,7 @@
 from python-flask-server.openapi_server.utils.constants import *
 import unittest
 
-class TestPagination(unittest.TestCase):
+class TestParameters(unittest.TestCase):
     
     def test_default_parameters(self):
         self.assertEqual(pageNumber, 1)
@@ -9,31 +9,31 @@ class TestPagination(unittest.TestCase):
         self.assertEqual(sortBy, 'id')
         self.assertEqual(sortDir, 'asc')
         self.assertEqual(search, '')
-    
-    def test_page_number(self):
-        new_page_number = 2
-        self.assertNotEqual(pageNumber, new_page_number)
-        self.assertEqual(new_page_number, PARAM_PAGE_NUMBER)
-    
-    def test_page_size(self):
-        new_page_size = 50
-        self.assertNotEqual(pageSize, new_page_size)
-        self.assertEqual(new_page_size, PARAM_PAGE_SIZE)
         
-    def test_sort_by(self):
-        new_sort_by = 'name'
-        self.assertNotEqual(sortBy, new_sort_by)
-        self.assertEqual(new_sort_by, PARAM_SORT_BY)
-    
-    def test_sort_dir(self):
-        new_sort_dir = 'desc'
-        self.assertNotEqual(sortDir, new_sort_dir)
-        self.assertEqual(new_sort_dir, PARAM_SORT_DIR)
-    
-    def test_search(self):
-        new_search = 'test'
-        self.assertNotEqual(search, new_search)
-        self.assertEqual(new_search, PARAM_SEARCH)
-    
+    def test_param_page_number(self):
+        global pageNumber
+        pageNumber = 2
+        self.assertEqual(pageNumber, 2)
+        
+    def test_param_page_size(self):
+        global pageSize
+        pageSize = 50
+        self.assertEqual(pageSize, 50)
+        
+    def test_param_sort_by(self):
+        global sortBy
+        sortBy = 'name'
+        self.assertEqual(sortBy, 'name')
+        
+    def test_param_sort_dir(self):
+        global sortDir
+        sortDir = 'desc'
+        self.assertEqual(sortDir, 'desc')
+        
+    def test_param_search(self):
+        global search
+        search = 'test'
+        self.assertEqual(search, 'test')
+        
 if __name__ == '__main__':
     unittest.main()
