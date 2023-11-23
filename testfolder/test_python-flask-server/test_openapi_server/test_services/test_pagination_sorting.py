a68dd5e821ec43bbe20da581c320f026bcc1aff2
Anurag Jain <anurag_jain1@persistent.com>
2023-11-23 13:27:34 +0530

    TestGen generated unit tests - 2023-11-23 13:27

--- testfolder/test_python-flask-server/test_openapi_server/test_services/test_pagination_sorting.py
+++ testfolder/test_python-flask-server/test_openapi_server/test_services/test_pagination_sorting.py
@@ -1,52 +1,49 @@
 from python-flask-server.openapi_server.services.pagination_sorting import *
 import unittest
-from unittest.mock import Mock, patch
-from flask import Flask, request
-from openapi_server.utils.constants import *
-from openapi_server.services.searching import searching
-from openapi_server import pagination_sorting
+from flask import Flask
 
 class TestPaginationSorting(unittest.TestCase):
-
+    
     def setUp(self):
         self.app = Flask(__name__)
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        self.app_context.pop()
 
     def test_pagination_sorting(self):
-        with self.app.test_request_context('/'):
-            # Set up mock values for request arguments
-            request.args = Mock()
-            request.args.get.side_effect = lambda x, default, type: {
-                PARAM_PAGE_NUMBER: 2,
-                PARAM_PAGE_SIZE: 10,
-                PARAM_SORT_BY: 'name',
-                PARAM_SORT_DIR: 'asc'
-            }.get(x, default)
+        response = self.client.get('/pagination_sorting?page_number=1&page_size=10&sort_by=id&sort_dir=asc')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json, list)
+        self.assertLessEqual(len(response.json), 10)
+        self.assertTrue(all(isinstance(item, dict) for item in response.json))
+        self.assertTrue(all('id' in item for item in response.json))
 
-            # Set up mock values for constants
-            pageNumber = 1
-            pageSize = 20
-            sortBy = 'id'
-            sortDir = 'desc'
+    def test_pagination_sorting_invalid_sort_dir(self):
+        response = self.client.get('/pagination_sorting?page_number=1&page_size=10&sort_by=id&sort_dir=invalid')
+        self.assertEqual(response.status_code, 400)
 
-            # Set up mock values for Model and search_params
-            Model = Mock()
-            search_params = 'test'
+    def test_pagination_sorting_search_params(self):
+        response = self.client.get('/pagination_sorting?page_number=1&page_size=10&sort_by=id&sort_dir=asc&search=test')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json, list)
+        self.assertTrue(all(isinstance(item, dict) for item in response.json))
+        self.assertTrue(all('test' in item.values() for item in response.json))
 
-            # Set up mock values for query object and items
-            query = Mock()
-            query.items = [{'id': 1, 'name': 'test1'}, {'id': 2, 'name': 'test2'}]
+    def test_pagination_sorting_invalid_page_number(self):
+        response = self.client.get('/pagination_sorting?page_number=invalid&page_size=10&sort_by=id&sort_dir=asc')
+        self.assertEqual(response.status_code, 400)
 
-            # Patch the searching function to return a query object
-            with patch('openapi_server.services.searching.searching') as mock_searching:
-                mock_searching.return_value = q = Mock()
-                q.order_by.return_value = sorted = Mock()
-                sorted.desc.return_value = 'desc'
-                sorted.asc.return_value = 'asc'
-                q.paginate.return_value = query
+    def test_pagination_sorting_invalid_page_size(self):
+        response = self.client.get('/pagination_sorting?page_number=1&page_size=invalid&sort_by=id&sort_dir=asc')
+        self.assertEqual(response.status_code, 400)
 
-                # Call the pagination_sorting function and check the results
-                result = pagination_sorting(Model)
-                self.assertEqual(result, [{'id': 1, 'name': 'test1'}, {'id': 2, 'name': 'test2'}])
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_pagination_sorting_missing_params(self):
+        response = self.client.get('/pagination_sorting')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json, list)
+        self.assertLessEqual(len(response.json), 10)
+        self.assertTrue(all(isinstance(item, dict) for item in response.json))
+        self.assertTrue(all('id' in item for item in response.json))
--- testfolder/test_python-flask-server/test_openapi_server/test_utils/test_constants.py
+++ testfolder/test_python-flask-server/test_openapi_server/test_utils/test_constants.py
@@ -1,39 +1,42 @@
 from python-flask-server.openapi_server.utils.constants import *
 import unittest
 
-class TestParameters(unittest.TestCase):
-    
+class TestPagination(unittest.TestCase):
     def test_default_parameters(self):
         self.assertEqual(pageNumber, 1)
         self.assertEqual(pageSize, 100)
         self.assertEqual(sortBy, 'id')
         self.assertEqual(sortDir, 'asc')
         self.assertEqual(search, '')
-        
-    def test_param_page_number(self):
-        global pageNumber
-        pageNumber = 2
-        self.assertEqual(pageNumber, 2)
-        
-    def test_param_page_size(self):
-        global pageSize
-        pageSize = 50
-        self.assertEqual(pageSize, 50)
-        
-    def test_param_sort_by(self):
-        global sortBy
-        sortBy = 'name'
-        self.assertEqual(sortBy, 'name')
-        
-    def test_param_sort_dir(self):
-        global sortDir
-        sortDir = 'desc'
-        self.assertEqual(sortDir, 'desc')
-        
-    def test_param_search(self):
-        global search
-        search = 'test'
-        self.assertEqual(search, 'test')
-        
+
+    def test_custom_parameters(self):
+        custom_pageNumber = 2
+        custom_pageSize = 50
+        custom_sortBy = 'name'
+        custom_sortDir = 'desc'
+        custom_search = 'example'
+
+        self.assertEqual(custom_pageNumber, 2)
+        self.assertEqual(custom_pageSize, 50)
+        self.assertEqual(custom_sortBy, 'name')
+        self.assertEqual(custom_sortDir, 'desc')
+        self.assertEqual(custom_search, 'example')
+
+    def test_missing_parameters(self):
+        self.assertRaises(TypeError, function_name)
+
+    def test_invalid_parameters(self):
+        invalid_pageNumber = 'invalid'
+        invalid_pageSize = 'invalid'
+        invalid_sortBy = 123
+        invalid_sortDir = True
+        invalid_search = None
+
+        self.assertRaises(TypeError, function_name, invalid_pageNumber, pageSize, sortBy, sortDir, search)
+        self.assertRaises(TypeError, function_name, pageNumber, invalid_pageSize, sortBy, sortDir, search)
+        self.assertRaises(TypeError, function_name, pageNumber, pageSize, invalid_sortBy, sortDir, search)
+        self.assertRaises(TypeError, function_name, pageNumber, pageSize, sortBy, invalid_sortDir, search)
+        self.assertRaises(TypeError, function_name, pageNumber, pageSize, sortBy, sortDir, invalid_search)
+
 if __name__ == '__main__':
     unittest.main()
