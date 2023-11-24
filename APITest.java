9a76c9d25cb3d82ea5e5451f5827291341e9cb21
Anurag Jain <anurag_jain1@persistent.com>
2023-11-24 12:04:06 +0530

    APITesting generated file - 2023-11-24 12:03

--- APITest.java
+++ APITest.java
@@ -1,101 +1,93 @@
 import io.restassured.RestAssured;
-import io.restassured.authentication.AuthenticationScheme;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
 
-public class APITest {
+public class ApiTests {
 
-    // Test 1
     @Test
-    public void testGetUser() {
+    public void testGetUserById() {
         RestAssured.baseURI = "https://reqres.in/api";
         given()
                 .when()
                 .get("/users/2")
                 .then()
-                .assertThat().statusCode(200)
+                .statusCode(200)
                 .body("data.first_name", equalTo("Janet"));
     }
 
-    // Test 2
     @Test
     public void testCreateUser() {
         RestAssured.baseURI = "https://reqres.in/api";
-        String request = "{\"name\": \"Sam\", \"job\": \"Project Leader\"}";
         given()
-                .header("Content-Type", "application/json")
-                .body(request)
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"Sam\", \"job\": \"Project Leader\"}")
                 .when()
                 .post("/users")
                 .then()
-                .assertThat().statusCode(201)
+                .statusCode(201)
                 .body("name", equalTo("Sam"))
                 .body("job", containsString("Leader"));
     }
 
-    // Test 3
     @Test
-    public void testUpdateUser() {
+    public void testUpdateUserById() {
         RestAssured.baseURI = "https://reqres.in/api";
-        String request = "{\"name\": \"Isha\", \"job\": \"Software Engineer\"}";
         given()
-                .header("Content-Type", "application/json")
-                .body(request)
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"Isha\", \"job\": \"Software Engineer\"}")
                 .when()
                 .put("/users/2")
                 .then()
-                .assertThat().statusCode(200);
+                .statusCode(200);
     }
 
-    // Test 4
     @Test
-    public void testDeleteUser() {
+    public void testDeleteUserById() {
         RestAssured.baseURI = "https://reqres.in/api";
         given()
                 .when()
                 .delete("/users/2")
                 .then()
-                .assertThat().statusCode(204);
+                .statusCode(204);
     }
 
-    // Test 5
     @Test
-    public void testGetUsersWithPageQueryParameter() {
+    public void testGetUsersByPage() {
         RestAssured.baseURI = "https://reqres.in/api";
         given()
                 .param("page", "2")
                 .when()
                 .get("/users")
                 .then()
-                .assertThat().statusCode(200);
+                .statusCode(200);
     }
 
-    // Test 6
     @Test
-    public void testGetUserWithIdPathParam() {
+    public void testGetUserByIdWithPathParam() {
         RestAssured.baseURI = "https://reqres.in/api";
         given()
                 .pathParam("id", "3")
                 .when()
                 .get("/users/{id}")
                 .then()
-                .assertThat().statusCode(200)
+                .statusCode(200)
                 .body("data.last_name", equalTo("Wong"));
     }
 
-    // Test 7
     @Test
-    public void testRegisterUserWithBasicAuth() {
+    public void testRegisterWithBasicAuth() {
         RestAssured.baseURI = "https://reqres.in/api";
         given()
-                .auth().basic("username", "password")
+                .auth()
+                .preemptive()
+                .basic("username", "password")
                 .when()
                 .post("/register")
                 .then()
-                .assertThat().statusCode(400);
+                .statusCode(400);
     }
 }
