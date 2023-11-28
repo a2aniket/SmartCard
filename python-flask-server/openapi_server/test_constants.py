7768a1f89313df87101214e833a88918febd815e
Anurag Jain <anurag_jain1@persistent.com>
2023-11-28 15:50:36 +0530

    TestGen generated unit tests - 2023-11-28 15:50

--- python-flask-server/openapi_server/test_constants.py
+++ python-flask-server/openapi_server/test_constants.py
@@ -2,41 +2,37 @@ from utils.constants import *
 import unittest
 
 class TestParameters(unittest.TestCase):
-    
-    def test_default_pageNumber(self):
+    def test_default_parameters(self):
         self.assertEqual(pageNumber, 1)
-        
-    def test_default_pageSize(self):
         self.assertEqual(pageSize, 100)
-        
-    def test_default_sortBy(self):
         self.assertEqual(sortBy, 'id')
-        
-    def test_default_sortDir(self):
         self.assertEqual(sortDir, 'asc')
-        
-    def test_default_search(self):
         self.assertEqual(search, '')
-        
-    def test_custom_pageNumber(self):
-        custom_pageNumber = 5
-        self.assertEqual(custom_pageNumber, 5)
-        
-    def test_custom_pageSize(self):
-        custom_pageSize = 50
-        self.assertEqual(custom_pageSize, 50)
-        
-    def test_custom_sortBy(self):
-        custom_sortBy = 'name'
-        self.assertEqual(custom_sortBy, 'name')
-        
-    def test_custom_sortDir(self):
-        custom_sortDir = 'desc'
-        self.assertEqual(custom_sortDir, 'desc')
-        
-    def test_custom_search(self):
-        custom_search = 'example'
-        self.assertEqual(custom_search, 'example')
-        
+
+    def test_page_number_parameter(self):
+        global pageNumber
+        pageNumber = 5
+        self.assertEqual(pageNumber, 5)
+
+    def test_page_size_parameter(self):
+        global pageSize
+        pageSize = 50
+        self.assertEqual(pageSize, 50)
+
+    def test_sort_by_parameter(self):
+        global sortBy
+        sortBy = 'name'
+        self.assertEqual(sortBy, 'name')
+
+    def test_sort_dir_parameter(self):
+        global sortDir
+        sortDir = 'desc'
+        self.assertEqual(sortDir, 'desc')
+
+    def test_search_parameter(self):
+        global search
+        search = 'test'
+        self.assertEqual(search, 'test')
+
 if __name__ == '__main__':
     unittest.main()
