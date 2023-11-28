449a8d1993dff6e04af1e5a01b144a531386c3e7
Anurag Jain <anurag_jain1@persistent.com>
2023-11-28 15:48:21 +0530

    TestGen generated unit tests - 2023-11-28 15:48

--- python-flask-server/openapi_server/test_constants.py
+++ python-flask-server/openapi_server/test_constants.py
@@ -1,31 +1,42 @@
 from utils.constants import *
 import unittest
 
-class TestDefaultParams(unittest.TestCase):
-
-    def test_default_params(self):
+class TestParameters(unittest.TestCase):
+    
+    def test_default_pageNumber(self):
         self.assertEqual(pageNumber, 1)
+        
+    def test_default_pageSize(self):
         self.assertEqual(pageSize, 100)
+        
+    def test_default_sortBy(self):
         self.assertEqual(sortBy, 'id')
+        
+    def test_default_sortDir(self):
         self.assertEqual(sortDir, 'asc')
+        
+    def test_default_search(self):
         self.assertEqual(search, '')
-
-class TestParams(unittest.TestCase):
-
-    def test_page_number(self):
-        self.assertEqual(PARAM_PAGE_NUMBER, 'pageNumber')
-
-    def test_page_size(self):
-        self.assertEqual(PARAM_PAGE_SIZE, 'pageSize')
-
-    def test_sort_by(self):
-        self.assertEqual(PARAM_SORT_BY, 'sortBy')
-
-    def test_sort_dir(self):
-        self.assertEqual(PARAM_SORT_DIR, 'sortDir')
-
-    def test_search(self):
-        self.assertEqual(PARAM_SEARCH, 'search')
-
+        
+    def test_custom_pageNumber(self):
+        custom_pageNumber = 5
+        self.assertEqual(custom_pageNumber, 5)
+        
+    def test_custom_pageSize(self):
+        custom_pageSize = 50
+        self.assertEqual(custom_pageSize, 50)
+        
+    def test_custom_sortBy(self):
+        custom_sortBy = 'name'
+        self.assertEqual(custom_sortBy, 'name')
+        
+    def test_custom_sortDir(self):
+        custom_sortDir = 'desc'
+        self.assertEqual(custom_sortDir, 'desc')
+        
+    def test_custom_search(self):
+        custom_search = 'example'
+        self.assertEqual(custom_search, 'example')
+        
 if __name__ == '__main__':
     unittest.main()
